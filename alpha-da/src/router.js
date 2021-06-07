e4b19d0c2be5a165c359fce52c1fc60fb8ad3929
hackathon IDF <hack@cs.colman.ac.il>
2021-06-07 14:03:35 +0300

    minor chnges

--- alpha-da/src/router.js
+++ alpha-da/src/router.js
@@ -1,14 +1,17 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 
-import HomePage from '../pages/HomePage.vue'
+import HomePage from '../pages/HomePage.vue';
+import LoginPage from '../pages/LoginPage.vue'
+
 
 Vue.use(Router);
 
 export const router = new Router({
     mode: 'history',
     routes: [
-        { path: '/', component: HomePage },
+        { path: '/', component: LoginPage },
+        { path: '/home', component: HomePage },
 
         // otherwise redirect to home
         { path: '*', redirect: '/' }
