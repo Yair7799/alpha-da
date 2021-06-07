3c457b59f1b848d339f9a2c936bf0e4f88b33b55
hackathon IDF <hack@cs.colman.ac.il>
2021-06-07 10:56:27 +0300

    added card shell

--- alpha-da/src/components/intelj.vue
+++ alpha-da/src/components/intelj.vue
@@ -8,31 +8,57 @@
   >
     <v-list-item three-line>
       <v-list-item-content>
-        <h1>
+        <h2>
           ציר מודיעיני
-        </h1>
+        </h2>
         <v-list-item-title class="text-h5 mb-1">
-          Headline 5
+           <v-btn
+        outlined
+          style="height:25%; width:30%"      
+      >
+        חשודים
+      </v-btn>
+         <v-btn
+        outlined
+        style="height:25%; width:30%"
+
+        
+      >
+        מבוקשים
+      </v-btn>
         </v-list-item-title>
-        <v-list-item-subtitle>Greyhound divisely hello coldly fonwderfully</v-list-item-subtitle>
+      
       </v-list-item-content>
 
-      <v-list-item-avatar
-        tile
-        size="80"
-        color="grey"
-      ></v-list-item-avatar>
+   
     </v-list-item>
 
-    <v-card-actions>
-      <v-btn
-        outlined
-        rounded
-        text
-      >
-        Button
-      </v-btn>
-    </v-card-actions>
+    <div style="margin-right:5%; margin-left:8%; ">
+
+     <v-row >
+        <v-col md="12" style="padding-top:1%">
+     <div style="background-color:#070129" >
+        <v-row>
+       
+            <v-img
+                style="padding-right:5%"
+                 max-height="148"
+                 max-width="100"
+                src="https://picsum.photos/id/11/500/300"
+           ></v-img>
+        <v-col>
+        <p style="color:white; margin-bottom:0%;" >גו'ן סמית </p>            
+        <p style="color:#E7E7E7; margin-bottom:2%;">0502234566 </p>
+        </v-col>
+        </v-row>
+     </div>
+     </v-col>
+    </v-row>
+
+    </div>
+    
+
+ 
   </v-card>
 
 </template>
