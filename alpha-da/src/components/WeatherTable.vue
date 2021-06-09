40737c3dfe707ab0f52920bdf9c582d2cded196f
hackathon IDF <hack@cs.colman.ac.il>
2021-06-09 13:50:41 +0300

    feat(weatherTable.vue): updated temp numbers

--- alpha-da/src/components/WeatherTable.vue
+++ alpha-da/src/components/WeatherTable.vue
@@ -22,20 +22,146 @@ export default {
       temperature: 270,
       date: new Date(),
       temps: [
-        { temp: 0, pressure: 1012, humidity: 30 },
-        { temp: 3, pressure: 1012, humidity: 95 },
-        { temp: 2, pressure: 1000, humidity: 40 },
-        { temp: 80, pressure: 1012, humidity: 30 },
-        { temp: 2, pressure: 1012, humidity: 95 },
-        { temp: 2, pressure: 1000, humidity: 40 },
-        { temp: 80, pressure: 1012, humidity: 30 },
-        { temp: 270, pressure: 1012, humidity: 95 },
-        { temp: 270, pressure: 1000, humidity: 40 },
-        { temp: 80, pressure: 1012, humidity: 30 },
-        { temp: 270, pressure: 1012, humidity: 95 },
-        { temp: 270, pressure: 1000, humidity: 40 },
-        { temp: 80, pressure: 1012, humidity: 30 },
-        { temp: 270, pressure: 1000, humidity: 40 }
+        {
+          temp: 96.59,
+          feels_like: 96.35,
+          temp_min: 92.75,
+          temp_max: 97.36,
+          pressure: 1012,
+          humidity: 52,
+          sea_level: 1012,
+          grnd_level: 924
+        },
+        {
+          temp: 60.59,
+          feels_like: 60.35,
+          temp_min: 60.75,
+          temp_max: 60.36,
+          pressure: 1012,
+          humidity: 30,
+          sea_level: 1012,
+          grnd_level: 924
+        },
+        {
+          temp: 70.59,
+          feels_like: 70.35,
+          temp_min: 65.75,
+          temp_max: 77.36,
+          pressure: 900,
+          humidity: 30,
+          sea_level: 1012,
+          grnd_level: 924
+        },
+        {
+          temp: 100.59,
+          feels_like: 100.35,
+          temp_min: 100.75,
+          temp_max: 100.36,
+          pressure: 1012,
+          humidity: 80,
+          sea_level: 1012,
+          grnd_level: 924
+        },
+        {
+          temp: 70.59,
+          feels_like: 70.35,
+          temp_min: 65.75,
+          temp_max: 277.36,
+          pressure: 900,
+          humidity: 30,
+          sea_level: 1012,
+          grnd_level: 924
+        },
+        {
+          temp: 51.59,
+          feels_like: 51.35,
+          temp_min: 51.75,
+          temp_max: 41.36,
+          pressure: 1012,
+          humidity: 15,
+          sea_level: 1012,
+          grnd_level: 924
+        },
+        {
+          temp: 90.59,
+          feels_like: 90.35,
+          temp_min: 90.75,
+          temp_max: 90.36,
+          pressure: 1024,
+          humidity: 40,
+          sea_level: 1012,
+          grnd_level: 924
+        },
+        {
+          temp: 60.59,
+          feels_like: 60.35,
+          temp_min: 60.75,
+          temp_max: 60.36,
+          pressure: 1012,
+          humidity: 97,
+          sea_level: 1012,
+          grnd_level: 924
+        },
+        {
+          temp: 80.59,
+          feels_like: 80.35,
+          temp_min: 75.75,
+          temp_max: 85.36,
+          pressure: 1012,
+          humidity: 30,
+          sea_level: 1012,
+          grnd_level: 924
+        },
+        {
+          temp: 68.59,
+          feels_like: 68.35,
+          temp_min: 63.75,
+          temp_max: 68.36,
+          pressure: 1012,
+          humidity: 30,
+          sea_level: 1012,
+          grnd_level: 924
+        },
+        {
+          temp: 70.59,
+          feels_like: 70.35,
+          temp_min: 65.75,
+          temp_max: 77.36,
+          pressure: 1012,
+          humidity: 30,
+          sea_level: 1012,
+          grnd_level: 924
+        },
+        {
+          temp: 70.59,
+          feels_like: 70.35,
+          temp_min: 65.75,
+          temp_max: 77.36,
+          pressure: 1012,
+          humidity: 100,
+          sea_level: 1012,
+          grnd_level: 924
+        },
+        {
+          temp: 70.59,
+          feels_like: 70.35,
+          temp_min: 65.75,
+          temp_max: 77.36,
+          pressure: 1012,
+          humidity: 70,
+          sea_level: 1012,
+          grnd_level: 924
+        },
+        {
+          temp: 70.59,
+          feels_like: 70.35,
+          temp_min: 65.75,
+          temp_max: 77.36,
+          pressure: 1012,
+          humidity: 80,
+          sea_level: 1012,
+          grnd_level: 924
+        }
       ]
     };
   },
